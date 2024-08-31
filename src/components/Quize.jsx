611f29afff281aa91e029e752f283ee0b4527a66
manzitresor <manzitre@gmail.com>
2024-08-31 21:39:35 +0200

    feat: add quize completed feature

--- src/components/Quize.jsx
+++ src/components/Quize.jsx
@@ -1,18 +1,31 @@
 import { useState } from "react"
 import QUESTION from "../question.js";
+import quizCompletedImg from "../assets/quiz-complete.png"
 
 export default function Quize() {
     const [selectAnswer, setSelectAnswer ] = useState([]);
 
     const activeQuestionIndex = selectAnswer.length;
-    const shuffledAnswers = QUESTION[activeQuestionIndex].answers;
-    shuffledAnswers.sort(() => Math.random() - 0.5)
+    const quizIsCompled = activeQuestionIndex === QUESTION.length;
 
     function handleSelectAnswer(selectedAnswer) {
         setSelectAnswer(prev => {
-            return [...prev,selectAnswer]
+            return [...prev,selectedAnswer]
         })
     }
+
+    if (quizIsCompled) {
+        return(
+            <div id="summary">
+                <img src={quizCompletedImg} alt="quize complete" />
+                <h2>Quiz completed</h2>
+            </div>
+        )
+    }
+
+    const shuffledAnswers = QUESTION[activeQuestionIndex].answers;
+    shuffledAnswers.sort(() => Math.random() - 0.5);
+
   return (
     <div id="quiz">
        <div id="question">
