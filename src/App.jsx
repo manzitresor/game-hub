e821bd639e135a704b60baf4397284d5be01e5b0
manzitresor <manzitre@gmail.com>
2024-08-31 21:27:50 +0200

    chore: add quize page in app to be diplayed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import Quize from "./components/Quize";
 
 function App() {
     return(
         <>
         <Header/>
+        <Quize/>
         </>
     )
 }
